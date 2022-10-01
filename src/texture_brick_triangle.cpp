6bbe93264d7b37e3d834ba6cdb0919475d589605
SFujiN <D0n4lddanghuynh@gmail.com>
2022-10-01 17:54:43 -0500

    brick texture triangle

--- src/texture_brick_triangle.cpp
+++ src/texture_brick_triangle.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb_image.h>
 
 #include "shader_s.h"
 
@@ -35,13 +36,13 @@ int main()
 		return -1;
 	}
 
-	Shader ourShader("../shaders/shader.vs", "../shaders/shader.fs");
+	Shader ourShader("../shaders/shader_wall.vs", "../shaders/shader_wall.fs");
 
 	float vertices[] = {
-		// positions		 // colors
-		 0.0f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f,	// top
-		 0.5f, -0.5f, 0.0f,	 1.0f, 1.0f, 1.0f,	// bottom right
-		-0.5f, -0.5f, 0.0f,	 1.0f, 1.0f, 1.0f	// bottom left
+		// positions		 // colors		// texture coords
+		 0.0f,  0.5f, 0.0f,  	 0.0f, 0.0f, 0.0f,	0.5f, 1.0f,	// top
+		 0.5f, -0.5f, 0.0f,	 1.0f, 1.0f, 1.0f,	1.0f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f,	 1.0f, 1.0f, 1.0f,	0.0f, 0.0f	// bottom left
 	};
 	unsigned int indices[] = {
 		0, 1, 2				// first triangle
@@ -61,11 +62,40 @@ int main()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0);
 	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3* sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// textures
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("../textures/wall.jpg", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
@@ -74,6 +104,8 @@ int main()
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindTexture(GL_TEXTURE_2D, texture);
 
 		ourShader.use();
 

4c21872eb31b1bef3d1291dbecae17879c644fd9
SFujiN <d0n4lddanghuynh@gmail.com>
2024-07-30 11:05:02 -0500

    Change Coordinate System window labels

--- src/coordinate_system.cpp
+++ src/coordinate_system.cpp
@@ -23,7 +23,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello Triangle", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Coordinate System", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;

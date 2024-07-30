ca0cd45ecea39b5623d014af5c25789fbb362513
SFujiN <d0n4lddanghuynh@gmail.com>
2024-07-30 17:38:29 -0500

    Finish up Phong lighting

--- src/basic_lighting.cpp
+++ src/basic_lighting.cpp
@@ -147,7 +147,6 @@ int main()
     ourShader.use();
     ourShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
     ourShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-    ourShader.setVec3("lightPos", lightPos);
 
 		// create transformations
 		// model matrix
@@ -160,13 +159,18 @@ int main()
 		// render boxes
     glm::mat4 model = glm::mat4(1.0f);
     ourShader.setMat4("model", model);
+    ourShader.setVec3("lightPos", lightPos);
+    ourShader.setVec3("viewPos", camera.Position);
 		glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
     lightCubeShader.use();
     lightCubeShader.setMat4("projection", projection);
     lightCubeShader.setMat4("view", view);
+    lightCubeShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
     model = glm::mat4(1.0f);
+    lightPos.x = 2.0f * cos(glfwGetTime());
+    lightPos.z = 2.0f * sin(glfwGetTime());
     model = glm::translate(model, lightPos);
     model = glm::scale(model, glm::vec3(0.2f));
     lightCubeShader.setMat4("model", model);

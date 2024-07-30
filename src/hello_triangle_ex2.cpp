9cebfb724d7df47a59837fc5457c679e5c627efd
SFujiN <d0n4lddanghuynh@gmail.com>
2024-07-30 11:19:20 -0500

    Change Hello Triangle window labels

--- src/hello_triangle_ex2.cpp
+++ src/hello_triangle_ex2.cpp
@@ -28,7 +28,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Triangle Exercise 2", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Triangle: Exercise 2", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;

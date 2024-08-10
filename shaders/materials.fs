5b709feac7462cda08e494a204c9fa8689344ae8
SFujiN <d0n4lddanghuynh@gmail.com>
2024-08-09 20:15:04 -0500

    Fix materials fragment shader

--- shaders/materials.fs
+++ shaders/materials.fs
@@ -4,7 +4,6 @@ in vec3 FragPos;
 in vec3 Normal;
 out vec4 FragColor;
 
-uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 struct Material {
@@ -32,7 +31,7 @@ void main() {
 
   // diffuse
   vec3 norm = normalize(Normal);
-  vec3 lightDir = normalize(lightPos - FragPos);
+  vec3 lightDir = normalize(light.position - FragPos);
   float diff = max(dot(norm, lightDir), 0.0);
   vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
